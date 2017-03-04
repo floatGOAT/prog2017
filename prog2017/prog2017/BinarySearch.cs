fd793e8872562b698a672a01b910db735258de60
floatGOAT <pandarpr@yandex.ru>
2017-03-04 11:52:31 +0600

    Task: binary search
    Implement algorithm searching a number in array and some tests for this
    program.

---
+++ prog2017/prog2017/BinarySearch.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2017
+{
+    class BinarySearch
+    {
+        private static int StartSearch(int[] array, int value)
+        {
+            // Checking for relevant input parametres
+            if (array.Length < 1 || array == null || array.Min() > value || array.Max() < value)
+                return -1;
+
+            // Algorythm of searching @value in @array
+            int low = 0;
+            int high = array.Length - 1;
+            int middle = (int)Math.Floor((low + high) / 2.0);
+            while (low < high)
+            {
+                middle = (int)Math.Floor((low + high) / 2.0);
+                if (array[middle] >= value)
+                    high = middle;
+                else
+                {
+                    low = middle + 1;
+                }
+            }
+            return high;
+        }
+        public static void SimpleTest()
+        {
+            // Testing on the simpliest test
+            int[] SimpleArray = new[] { 1, 2, 3, 4, 5 };
+            if (StartSearch(SimpleArray, 4) != 3)
+                Console.WriteLine("! Поиск не нашёл число 4 среди чисел {1, 2, 3, 4, 5}");
+            else
+                Console.WriteLine("Поиск на простом тесте работает корректно");
+        }
+        public static void TestNegativeNumbers()
+        {
+            // Testing search in negative numbers
+            int[] negativeNumbers = new[] { -5, -4, -3, -2 };
+            if (StartSearch(negativeNumbers, -3) != 2)
+                Console.WriteLine("! Поиск не нашёл число -3 среди чисел {-5, -4, -3,- 2}");
+            else
+                Console.WriteLine("Поиск среди отрицательных чисел работает корректно");
+        }
+        public static void TestNonExistentElement()
+        {
+            // Testing search for missing element
+            int[] negativeNumbers = new[] { -5, -4, -3, -2 };
+            if (StartSearch(negativeNumbers, -1) >= 0)
+                Console.WriteLine("! Поиск нашёл число -1 среди чисел {-5, -4, -3, -2}");
+            else
+                Console.WriteLine("Поиск отсутствующего элемента вернул корректный результат работает корректно");
+        }
+        public static void TestRepeatingNumbers()
+        {
+            // Testing search for repeating numbers in array
+            int[] repeatNumbers = new[] { -2, -1, 8, 8, 8, 15 };
+            if (StartSearch(repeatNumbers, 8) < 2 || StartSearch(repeatNumbers, 8) > 4)
+                Console.WriteLine("! Поиск не нашёл число 8 среди чисел {-2, 8, 3, 8, -1, 5}");
+            else
+                Console.WriteLine("Поиск среди повторяющихся чисел работает корректно");
+        }
+
+        public static void TestEmptyArray()
+        {
+            // Testing search in empty array
+            int[] emptyArray = new int[] { };
+            if (StartSearch(emptyArray, 42) != -1)
+                Console.WriteLine("! Поиск в пустом массиве работает некорректно");
+            else
+                Console.WriteLine("Поиск в пустом массиве работает корректно");
+        }
+        public static void StressTest()
+        {
+            // Testing search in huge array
+            const int numberOfNumbers = 100001;
+            bool temp = false;
+            int[] bigAmountOfNumbers = new int[numberOfNumbers];
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                bigAmountOfNumbers[i] = i;
+            }
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                if (StartSearch(bigAmountOfNumbers, i) != i)
+                    temp = true;
+            }
+            if (temp)
+                Console.WriteLine("! Стресс-тест не пройден");
+            else
+                Console.WriteLine("Стресс-тест пройден");
+        }
+    }
+}
---
+++ prog2017/prog2017/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace prog2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Game: guess a number*/
+            /* Manual managment */
+            // NumberGuess.ConsoleUserOpportunities();
+
+            /* One automaticaly test with auto-answering */
+            // AutoTest();
+
+            /* All tests in range [1..MAX_VALUE];
+               Create an array with numbers witch weren't guessed */
+            // TestForTesting();
+
+            /* WinForms interface testing */
+             Application.Run(new DialogWindow());
+
+            /* Binary search in array. Tests: */
+            /*
+            BinarySearch.SimpleTest();
+            BinarySearch.TestNegativeNumbers();
+            BinarySearch.TestNonExistentElement();
+            BinarySearch.TestRepeatingNumbers();
+            BinarySearch.TestEmptyArray();
+            BinarySearch.StressTest();
+            */
+
+            /* Provide pause for checking results of programms' work */
+            // Console.ReadKey();
+
+        }
+
+        static void AutoTest()
+        {
+            Console.WriteLine("Perform a number: ");
+            String s = Console.ReadLine();
+            int userNumber = int.Parse(s);
+            Console.Clear();
+            Console.WriteLine("Your number is {0}!", NumberGuess.StartGuess(userNumber));
+            Console.ReadKey();
+        }
+
+        static bool AutoTest(int userNumber)
+        {
+            return NumberGuess.StartGuess(userNumber) == userNumber;
+        }
+
+        static void TestForTesting()
+        {
+            int length = 0;
+            int[] wrongAnswers = new int[length];
+            for (int i = 1; i < MAX_VALUE; i++) 
+            {
+                if (!AutoTest(i))
+                {
+                    length++;
+                    Array.Resize<int>(ref wrongAnswers, length);
+                    wrongAnswers[length - 1] = i;
+                }
+            }
+            Console.WriteLine("wrong tests: ");
+            for (int i = 0; i < length; i++)
+                Console.WriteLine(wrongAnswers[i]);
+        }
+
+        public static int MAX_VALUE = 10000;
+    }
+}
